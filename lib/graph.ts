b2ce243312466755c8019be2b078c4535aa2f35f
udhaybegyall <udaybegyall12345@gmail.com>
2024-06-20 20:06:36 +0530

    optmized checkSquares method

--- lib/graph.ts
+++ lib/graph.ts
@@ -1,76 +1,78 @@
 type Dot = {
-    x: number;
-    y: number;
-}
+  x: number;
+  y: number;
+};
 
 class Graph {
-    private rows: number;
-    private cols: number;
-    private adjacencyList: Map<string, Set<string>>;
+  private rows: number;
+  private cols: number;
+  private adjacencyList: Map<string, Set<string>>;
 
-    constructor(rows: number, cols: number) {
-        this.rows = rows;
-        this.cols = cols;
-        this.adjacencyList = new Map();
-        this.initializeDots();
+  constructor(rows: number, cols: number) {
+    this.rows = rows;
+    this.cols = cols;
+    this.adjacencyList = new Map();
+    this.initializeDots();
+  }
+
+  private initializeDots() {
+    for (let x = 0; x < this.rows; x++) {
+      for (let y = 0; y < this.cols; y++) {
+        const key = this.getKey(x, y);
+        this.adjacencyList.set(key, new Set());
+      }
     }
+  }
 
-    private initializeDots() {
-        for (let x = 0; x < this.rows; x++) {
-            for (let y = 0; y < this.cols; y++) {
-                const key = this.getKey(x, y);
-                this.adjacencyList.set(key, new Set());
-            }
-        }
+  private getKey(x: number, y: number): string {
+    return `${x}-${y}`;
+  }
+
+  addEdge(dot1: Dot, dot2: Dot) {
+    const key1 = this.getKey(dot1.x, dot1.y);
+    const key2 = this.getKey(dot2.x, dot2.y);
+    if (this.adjacencyList.has(key1) && this.adjacencyList.has(key2)) {
+      this.adjacencyList.get(key1)?.add(key2);
+      this.adjacencyList.get(key2)?.add(key1);
     }
+  }
 
-    private getKey(x: number, y: number): string {
-        return `${x}-${y}`;
-    }
+  areNeighbors(dot1: Dot, dot2: Dot): boolean {
+    // const key1 = this.getKey(dot1.x, dot1.y);
+    // const key2 = this.getKey(dot2.x, dot2.y);
+    return (
+      (Math.abs(dot1.x - dot2.x) === 1 && dot1.y === dot2.y) ||
+      (Math.abs(dot1.y - dot2.y) === 1 && dot1.x === dot2.x)
+    );
+  }
 
-    addEdge(dot1: Dot, dot2: Dot) {
-        const key1 = this.getKey(dot1.x, dot1.y);
-        const key2 = this.getKey(dot2.x, dot2.y);
-        if (this.adjacencyList.has(key1) && this.adjacencyList.has(key2)) {
-            this.adjacencyList.get(key1)?.add(key2);
-            this.adjacencyList.get(key2)?.add(key1);
-        }
-    }
+  hasEdge(dot1: Dot, dot2: Dot): boolean {
+    const key1 = this.getKey(dot1.x, dot1.y);
+    const key2 = this.getKey(dot2.x, dot2.y);
+    return this.adjacencyList.get(key1)?.has(key2) || false;
+  }
 
-    areNeighbors(dot1: Dot, dot2: Dot): boolean {
-        // const key1 = this.getKey(dot1.x, dot1.y);
-        // const key2 = this.getKey(dot2.x, dot2.y);
-        return (
-          (Math.abs(dot1.x - dot2.x) === 1 && dot1.y === dot2.y) ||
-          (Math.abs(dot1.y - dot2.y) === 1 && dot1.x === dot2.x)
-        );
-    }
+  checkForSquares(dot: Dot): { x: number; y: number }[] {
+    // const squares: { x: number; y: number; }[] = [];
+    const { x, y } = dot;
 
-    hasEdge(dot1: Dot, dot2: Dot): boolean {
-        const key1 = this.getKey(dot1.x, dot1.y);
-        const key2 = this.getKey(dot2.x, dot2.y);
-        return this.adjacencyList.get(key1)?.has(key2) || false;
-    }
+    const potentialSquares = [
+      { x, y },
+      { x: x - 1, y },
+      { x, y: y - 1 },
+      { x: x - 1, y: y - 1 },
+    ];
 
-    checkForSquares(): { x: number; y: number }[] {
-        const squares = [];
-    
-        for (let x = 0; x < this.rows - 1; x++) {
-          for (let y = 0; y < this.cols - 1; y++) {
-            if (
-              this.hasEdge({ x, y }, { x: x + 1, y }) &&
-              this.hasEdge({ x, y }, { x, y: y + 1 }) &&
-              this.hasEdge({ x: x + 1, y }, { x: x + 1, y: y + 1 }) &&
-              this.hasEdge({ x, y: y + 1 }, { x: x + 1, y: y + 1 })
-            ) {
-              squares.push({ x, y });
-            }
-          }
-        }
-    
-        return squares;
-    }
-
+    return potentialSquares.filter(
+      ({ x, y }) =>
+        x >= 0 &&
+        y >= 0 &&
+        this.hasEdge({ x, y }, { x: x + 1, y }) &&
+        this.hasEdge({ x, y }, { x, y: y + 1 }) &&
+        this.hasEdge({ x: x + 1, y }, { x: x + 1, y: y + 1 }) &&
+        this.hasEdge({ x, y: y + 1 }, { x: x + 1, y: y + 1 })
+    );
+  }
 }
 
 export default Graph;
